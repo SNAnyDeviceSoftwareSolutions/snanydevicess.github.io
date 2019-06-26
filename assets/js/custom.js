e971151037283d33d7b85e030b73d771a37602cc
Sagar Kothari <sag333ar@gmail.com>
2019-06-26 06:23:40 +0530

    added link

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -25,7 +25,7 @@ $(document).ready(function(){
 		success:function(data)
 		{
 			$.each(data.value,function(index,value){
-				temp="<h4>"+ value.title +"</h4>"+"<p class='lead'>"+value.desc+"</p>";
+				temp="<a href='expensetrkr/index.html'><h4>"+ value.title +"</h4></a>"+"<p class='lead'>"+value.desc+"</p>";
 				$("#"+value.id).append(temp);
 			});
 		},
