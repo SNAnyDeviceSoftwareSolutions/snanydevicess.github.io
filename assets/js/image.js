31743ed0e270ec9ecaba2fb07a7a5786da843161
Sagar Kothari <sagar.k@turvo.com>
2019-08-13 19:02:20 +0530

    image pop-over css, js & html added

---
+++ assets/js/image.js
@@ -0,0 +1,18 @@
+
+function imageClicked(object) {
+  // Get the modal
+  var modal = document.getElementById("myModal");
+  // Get the image and insert it inside the modal - use its "alt" text as a caption
+  var img = document.getElementById("myImg");
+  var modalImg = document.getElementById("img01");
+  var captionText = document.getElementById("caption");
+  modal.style.display = "block";
+  modalImg.src = object.src;
+  captionText.innerHTML = object.alt;
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() { 
+    modal.style.display = "none";
+  }
+}
